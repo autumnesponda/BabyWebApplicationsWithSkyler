a49076e0459ce61078bc7f0554e29a1b6be46534
autumnesponda <autumnesponda@knights.ucf.edu>
2020-06-13 16:21:08 -0400

    added CatButton page to index; added bulma + xmlhttp to dependencies;

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("index");
 });
 
 module.exports = router;
